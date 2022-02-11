97df7bbb51162a4ed1658eab057d140e34e5fec5
Caio Dalo <caio.dalo@hotmail.com>
2022-02-10 21:42:17 -0300

    feat:first commit

---
+++ README.old.md
@@ -0,0 +1,2 @@
+# portfolio-react
+portfolio created using react.
---
+++ src/components/Header/index.js
@@ -0,0 +1,17 @@
+import '../Header/index.css'
+
+export default function Header() {
+    return (
+        <div className='header'>
+            <div className='header-container'>
+                <h3>Bem vindo!</h3>
+                <div className='menu'>
+                    <a href='#'>Sobre mim</a>
+                    <a href='#'>Trabalhos</a>
+                    <a href='#'>Contato</a>
+                    <a href='https://drive.google.com/uc?export=download&id=1TA_pWefRvNhifAceLlbxTAlGF5Aihomp'>Currículo</a>
+                </div>
+            </div>
+        </div>
+    )
+}
