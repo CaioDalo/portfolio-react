d61e329cbf674950ba9cc538cbcf02fc85fce84d
Caio Dalo <caio.dalo@hotmail.com>
2022-02-16 21:25:26 -0300

    fix:Ajustado header e adicionado informações de 'sobre mim'

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,14 +1,43 @@
 import '../Header/index.css'
 
+
 export default function Header() {
+
     return (
         <div className='header'>
             <div className='header-container'>
                 <h3>Bem vindo!</h3>
                 <div className='menu'>
-                    <a href='#' active>Sobre mim</a>
-                    <a href='#'>Trabalhos</a>
-                    <a href='#'>Contato</a>
+                    <div>
+                        <input
+                        defaultChecked
+                        id='about-me' 
+                        value='about-me' 
+                        name='checkbox-header' 
+                        type='radio' 
+                        />
+                        <label for='about-me'>Sobre mim</label>
+                    </div>
+
+                    <div>
+                        <input
+                        id='works' 
+                        value='works' 
+                        name='checkbox-header' 
+                        type='radio' 
+                        />
+                        <label for='works'>Trabalhos</label>
+                    </div>
+
+                    <div>
+                        <input
+                        id='contact' 
+                        value='contact' 
+                        name='checkbox-header' 
+                        type='radio' 
+                        />
+                        <label for='contact'>Contato</label>
+                    </div>
                     <a href='https://drive.google.com/uc?export=download&id=1TA_pWefRvNhifAceLlbxTAlGF5Aihomp'>Currículo</a>
                 </div>
             </div>
--- src/components/Informations/index.js
+++ src/components/Informations/index.js
@@ -1,16 +1,21 @@
 import '../Informations/index.css'
+import minhaImagem from '../../assets/caio.jpeg'
 
 export default function Informations() {
     return (
         <div className='informations'>
             <div className='informations-container-1'>
-                <img src></img>
+                <img src={minhaImagem} />
 
                 <h2>Caio Daló de Souza</h2>
 
-                <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer venenatis, tortor ut pretium aliquam, ipsum lectus laoreet lorem, condimentum tincidunt quam enim sed sem. Sed vulputate cursus sem ut ultricies. Sed a nibh sed libero semper volutpat vel sit amet arcu. In accumsan, dui sed eleifend tristique, nisl ligula rutrum purus, eget rutrum leo nisi vitae ligula. Curabitur blandit lectus bibendum eros molestie consequat. Phasellus tincidunt nisi quis aliquet pulvinar. Nunc pulvinar pretium ullamcorper. Ut sapien est, fringilla nec mollis quis, maximus sed nunc. Proin elit lectus, dictum nec semper sed, tempor non lectus. Donec congue lobortis ipsum at posuere.
-                </p>
+                
+                <p>Sempre fui apaixonado por tecnologia e no que ela era capaz de fazer, sendo ajudando quem precisasse ou simplesmente facilitando a vida das pessoas. Pensando nisso, resolvi trilhar esse caminho como desenvolvedor front-end (sim, migrei de profissão), e para isso, tenho estudado com afinco para manter o aprendizado contínuo com cursos e pequenos projetos pessoais, com os quais tenho desenvolvido habilidades em HTML5, CSS3, JavaScript, ReactJS dentre outros. Além disso, tenho o inglês como conhecimento fundamental, e sendo assim, tenho melhorado dia a dia minhas habilidades em leitura, fala e escrita, tendo como foco a fluência.</p>
+                <br />
+                <p>Tenho experiência em: HTML, CSS, JavaScript, Requisições Ajax, jQuery, Bootstrap, Sass, WordPress, Design responsivo e cultura Ágil (Métodos).</p>
+                <p>Estou estudando: ReactJS, Styled Components.</p>
+                <br />
+                <p>Sinta-se a vontade para entrar em contato!</p>
             </div>
         </div>
     )
